02c0fe570f38474f63bbe0e09649bc0f4417e5f3
PjBox <1169512743@qq.com>
2018-12-17 21:43:43 +0800

    添加RoleMapping

--- CMS.Data/CmsDbContext.cs
+++ CMS.Data/CmsDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,19 @@ namespace CMS.Data
 {
     public class CmsDbContext : System.Data.Entity.DbContext
     {
+        public CmsDbContext() : base("constr")
+        {
+
+        }
+
         public DbSet<Entities.User> Users { get; set; }
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(GetType().Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ CMS.Data/Mappings/RoleMapping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Mappings
+{
+    public class RoleMapping : EntityTypeConfiguration<Entities.Role>
+    {
+        public RoleMapping()
+        {
+            this.ToTable($"Sys{nameof(Entities.Role)}");
+            this.HasKey(_ => _.RoleId);
+            this.Property(_ => _.RoleName).HasMaxLength(25);
+            this.Property(_ => _.RoleCode).HasMaxLength(10);
+            this.Property(_ => _.Remark).HasMaxLength(50);
+        }
+    }
+}
